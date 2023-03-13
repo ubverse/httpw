58d7c914e91c00ff7ba47fc323ce6f1122eef225
Caian Ertl <caian.ertl@mzgroup.com>
2023-03-13 09:29:55 -0300

    feat: more request options/parameters and a better response object

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,101 @@
-import axios, { AxiosRequestConfig, Method } from 'axios'
+import axios, {
+  AxiosError,
+  AxiosRequestConfig,
+  AxiosResponseHeaders,
+  HttpStatusCode,
+  Method,
+  RawAxiosResponseHeaders,
+  ResponseType
+} from 'axios'
 
-export interface IHttpResponse<T = any> {
-  error: boolean
-  content: T
+interface StringMap {
+  [k: string]: string
+}
+
+type AwaitedResponse<T> = Promise<IHttpResponse<T>>
+
+export interface IHttpRequestConstructor {
+  timeout: number
+  baseUrl: string
+  headers: StringMap
+}
+
+export interface IHttpRequestExtraParams {
+  headers: StringMap
+  responseType: ResponseType
+  data: any
+  maxRequestLength: number
+  maxResponseLength: number
+}
+
+export interface IHttpResponse<T> {
+  hasError: boolean
+  statusCode: -1 | HttpStatusCode
+  headers: AxiosResponseHeaders | RawAxiosResponseHeaders
+  raw: {
+    request: AxiosRequestConfig
+    response?: AxiosResponse
+    error?: AxiosError
+  }
+  content?: T
 }
 
 export default class HttpRequest {
   protected timeout: number
-  protected headers: { [k: string]: string }
+  protected headers: StringMap
   protected baseUrl?: string
 
-  public constructor (timeout?: number) {
-    this.timeout = timeout ?? 10000
-    this.headers = {
-      'Content-Type': 'application/json'
-    }
+  public constructor (params?: Partial<IHttpRequestConstructor>) {
+    const { timeout = 10000, headers = {}, baseUrl } = params ?? {}
+
+    this.baseUrl = baseUrl
+    this.timeout = timeout
+    this.headers = Object.assign({ 'Content-Type': 'application/json' }, headers)
   }
 
-  private async perform<T>(payload: AxiosRequestConfig): Promise<IHttpResponse<T>> {
+  private async perform<T>(request: AxiosRequestConfig): AwaitedResponse<T> {
     const source = axios.CancelToken.source()
     setTimeout((): void => {
       source.cancel()
     }, this.timeout)
 
-    let error = false
-    let content: any
-
     try {
-      payload.cancelToken = source.token
-      content = await axios(payload)
-    } catch (e) {
-      error = true
-      content = e
-    }
+      request.cancelToken = source.token
+      const response = await axios(request)
 
-    return { error, content }
+      return {
+        hasError: false,
+        statusCode: response.status,
+        headers: response.headers,
+        content: response.data,
+        raw: { request, response }
+      }
+    } catch (e: any) {
+      return {
+        hasError: true,
+        statusCode: e?.response?.status ?? -1,
+        headers: e?.response?.headers ?? {},
+        content: e?.response?.data,
+        raw: { request, error: e }
+      }
+    }
   }
 
-  protected async request<T = any>(url: string, data?: any, method: Method = 'get'): Promise<IHttpResponse<T>> {
+  protected async request<T>(method: Method, url: string, params?: Partial<IHttpRequestExtraParams>): AwaitedResponse<T> {
+    const { headers = {}, responseType = 'json', maxRequestLength = Infinity, maxResponseLength = Infinity, data } = params ?? {}
+
+    // see: <https://axios-http.com/docs/req_config>
     const payload: AxiosRequestConfig = {
       url,
       method,
-      headers: this.headers,
-      maxContentLength: Infinity
+      responseType,
+      baseURL: this.baseUrl,
+      headers: Object.assign({}, this.headers, headers),
+      maxBodyLength: maxRequestLength,
+      maxContentLength: maxResponseLength
     }
 
-    if (typeof this.baseUrl !== 'undefined') {
-      payload.baseURL = this.baseUrl
-    }
-
-    if (typeof data !== 'undefined' && data !== null) {
+    if (data !== undefined && data !== null) {
       switch (method.toLowerCase()) {
         case 'get':
           payload.params = data
@@ -59,9 +105,12 @@ export default class HttpRequest {
         case 'post':
           payload.data = data
           break
+
+        default:
+          throw new Error(`Payload data is not supported for method ${method}`)
       }
     }
 
-    return await this.perform(payload)
+    return await this.perform<T>(payload)
   }
 }
